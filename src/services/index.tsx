220e16e890d6b97e2dec060ba2adce64d8518bac
JozielOliveira <programdor.joziel@gmail.com>
2020-05-30 17:01:58 -0300

    ajustment uri graphql

--- src/services/index.tsx
+++ src/services/index.tsx
@@ -2,7 +2,7 @@ import ApolloClient from "apollo-boost";
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
 export const client = new ApolloClient({
-  uri: `$https://api-aee.herokuapp.com/graphql`,
+  uri: `https://api-aee.herokuapp.com/graphql`,
   headers: {
     "Authorization": `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaXNBZG1pbiI6dHJ1ZSwiaWF0IjoxNTkwODY4NDQ5LCJleHAiOjE1OTM0NjA0NDl9.a3uRufs5fbPzT3rmDwn1iAMHN3Lnq-4Pg0VAjdfm8VI`
   },
