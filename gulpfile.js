6677b4ececd109d10a2b2bc353d89089e40ee9e1
JozielOliveira <programdor.joziel@gmail.com>
2020-05-30 16:01:15 -0300

    add dotenv

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,46 @@
+const dotenv = require('gulp-dotenv');
 const gulp = require('gulp')
 const gap = require('gulp-append-prepend')
 
-gulp.task('licenses', async function() {
+gulp.task('licenses', async function () {
 	await gulp
-		.src('build/static/js/*chunk.js', { base: './' })
+		.src('build/static/js/*chunk.js', {
+			base: './'
+		})
 		.pipe(
 			gap.prependText()
 		)
-		.pipe(gulp.dest('./', { overwrite: true }))
+		.pipe(gulp.dest('./', {
+			overwrite: true
+		}))
 
 	await gulp
-		.src('build/index.html', { base: './' })
+		.src('build/index.html', {
+			base: './'
+		})
 		.pipe(
 			gap.prependText()
 		)
-		.pipe(gulp.dest('./', { overwrite: true }))
+		.pipe(gulp.dest('./', {
+			overwrite: true
+		}))
 
 	await gulp
-		.src('build/static/css/*chunk.css', { base: './' })
+		.src('build/static/css/*chunk.css', {
+			base: './'
+		})
 		.pipe(
 			gap.prependText()
 		)
-		.pipe(gulp.dest('./', { overwrite: true }))
+		.pipe(gulp.dest('./', {
+			overwrite: true
+		}))
 	return
+})
+
+gulp.task('dotenv', function () {
+	return gulp.src('.env')
+		.pipe(dotenv())
+		.pipe(rename('env.json'))
+		.pipe(gulp.dest('dist'));
 })
--- src/services/index.tsx
+++ src/services/index.tsx
@@ -2,9 +2,9 @@ import ApolloClient from "apollo-boost";
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
 export const client = new ApolloClient({
-  uri: "http://localhost:1337/graphql",
+  uri: process.env.API_URL,
   headers: {
-    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaXNBZG1pbiI6dHJ1ZSwiaWF0IjoxNTkwMjczNjgxLCJleHAiOjE1OTI4NjU2ODF9.pzb92mBhGbgZSumCFHlOuhnZlzqYTdb863smsS5vFtQ"
+    "Authorization": `Bearer ${process.env.AUTH_TOKEN}`
   },
   cache: new InMemoryCache()
 });
