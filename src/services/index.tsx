9e7061da0b46859e57d8c1ff5edf17c1ece5c5a0
JozielOliveira <programdor.joziel@gmail.com>
2020-05-30 16:56:58 -0300

    add token

--- src/services/index.tsx
+++ src/services/index.tsx
@@ -2,9 +2,9 @@ import ApolloClient from "apollo-boost";
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
 export const client = new ApolloClient({
-  uri: process.env.API_URL,
+  uri: `$https://api-aee.herokuapp.com/graphql`,
   headers: {
-    "Authorization": `Bearer ${process.env.AUTH_TOKEN}`
+    "Authorization": `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaXNBZG1pbiI6dHJ1ZSwiaWF0IjoxNTkwODY4NDQ5LCJleHAiOjE1OTM0NjA0NDl9.a3uRufs5fbPzT3rmDwn1iAMHN3Lnq-4Pg0VAjdfm8VI`
   },
   cache: new InMemoryCache()
 });
