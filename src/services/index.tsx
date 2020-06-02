8e5e54125a69a8670dc357987558b5407994e894
JozielOliveira <programdor.joziel@gmail.com>
2020-06-02 15:06:52 -0300

    ajustment api url

--- src/services/index.tsx
+++ src/services/index.tsx
@@ -2,12 +2,11 @@ import ApolloClient from "apollo-boost";
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
 export const client = new ApolloClient({
-  uri: `http://localhost:1337/graphql`,
+  uri: `https://api-aee.herokuapp.com/graphql`,
   headers: {
-    "Authorization": `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaXNBZG1pbiI6dHJ1ZSwiaWF0IjoxNTkwMjczNjgxLCJleHAiOjE1OTI4NjU2ODF9.pzb92mBhGbgZSumCFHlOuhnZlzqYTdb863smsS5vFtQ`
+    "Authorization": `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaXNBZG1pbiI6dHJ1ZSwiaWF0IjoxNTkwODY4NDQ5LCJleHAiOjE1OTM0NjA0NDl9.a3uRufs5fbPzT3rmDwn1iAMHN3Lnq-4Pg0VAjdfm8VI`
   },
   cache: new InMemoryCache()
 });
 
 export * from "./quiz";
-export * from "./student";
