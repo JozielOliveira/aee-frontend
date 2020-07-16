75df88a0d0d8d4d92a36ca0a5839a577ca2d4077
JozielOliveira <programdor.joziel@gmail.com>
2020-07-16 18:54:47 -0300

    update config

--- src/services/config.ts
+++ src/services/config.ts
@@ -2,7 +2,7 @@ import ApolloClient from "apollo-boost";
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
 export const client = new ApolloClient({
-  uri: `http://localhost:1337/graphql`,
+  uri: `https://api-aee.herokuapp.com/graphql`,
   cache: new InMemoryCache(),
   request: ({ setContext }) => {
     setContext(({ headers }: any) => {
